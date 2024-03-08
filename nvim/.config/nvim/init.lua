c353c2ccaaea20eb38adf9bea7e66dfb803c9254
mc <mc@mc.com>
2024-03-08 23:22:29 +0800

    tow commit

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,5 +4,3 @@ require("config.keymaps")
 require("config.lazy")
 require("config.lsp")
 require("config.ibl")
-
--- vim.cmd.colorscheme("catppuccin-mocha")
--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,10 +1,10 @@
 local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-lspconfig["lua_ls"].setup({
-	capabilities = capabilities,
-})
-
-lspconfig["pyright"].setup({
-	capabilities = capabilities,
-})
+-- lspconfig["lua_ls"].setup({
+-- 	capabilities = capabilities,
+-- })
+--
+-- lspconfig["pyright"].setup({
+-- 	capabilities = capabilities,
+-- })
